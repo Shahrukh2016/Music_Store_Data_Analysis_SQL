bf4d6e95cab9347a3c1c0b05a9b6e717679ad785
Shahrukh Ahmad <115800947+Shahrukh2016@users.noreply.github.com>
2023-04-10 17:18:47 +0530

    Add files via upload

--- MusicStoreShahrukh.sql
+++ MusicStoreShahrukh.sql
@@ -1,5 +1,4 @@
-/*Basic Analysis*/
-/* */
+/* BASIC ANALYSIS */
 ------------------------------------------------------------------
 /* 1. Who is the senior most exployee based on job title? */
 SELECT FIRST_NAME, LAST_NAME FROM EMPLOYEE
@@ -18,16 +17,16 @@ ORDER BY TOTAL DESC
 LIMIT 3
 
 /* 4. Which city has the best customers?
-We would like to throw a promotional Music Festival in the city we made the most money.
-Write a query that returns one city that has the highest sum of invoice totals.
-Return both the city name & sum of all invoice totals */
+	  We would like to throw a promotional Music Festival in the city we made the most money.
+	  Write a query that returns one city that has the highest sum of invoice totals.
+	  Return both the city name & sum of all invoice totals */
 SELECT BILLING_CITY, SUM(TOTAL) FROM INVOICE
 GROUP BY BILLING_CITY
 ORDER BY SUM(TOTAL) DESC
 
 /* 5. Who is the best customer?
-The customer who has spent the most money will be declared the best customer.
-Write a query that returns the person who has spent the most money */
+	  The customer who has spent the most money will be declared the best customer.
+	  Write a query that returns the person who has spent the most money */
 SELECT C.CUSTOMER_ID, C.FIRST_NAME, C.LAST_NAME, SUM(I.TOTAL) AS TOTAL_SPENT
 FROM CUSTOMER AS C
 INNER JOIN INVOICE AS I
@@ -36,11 +35,10 @@ GROUP BY C.CUSTOMER_ID
 ORDER BY TOTAL_SPENT DESC
 LIMIT 1
 
-/* Morderate Analysis*/
-/* */
+/* MORDERATE ANALYSIS */
 ------------------------------------------------------------------
 /* 1. Write query to return the email, first name, last name, & Genre of all Rock Music listeners.
-Return your list ordered alphabetically by email starting with A. */
+	  Return your list ordered alphabetically by email starting with A. */
 SELECT EMAIL, FIRST_NAME, LAST_NAME FROM CUSTOMER
 JOIN INVOICE ON CUSTOMER.CUSTOMER_ID=INVOICE.CUSTOMER_ID
 JOIN INVOICE_LINE ON INVOICE.INVOICE_ID=INVOICE_LINE.INVOICE_ID
@@ -50,18 +48,31 @@ WHERE TRACK_ID IN (SELECT TRACK_ID FROM TRACK
 ORDER BY EMAIL ASC
 
 /* 2. Let's invite the artists who have written the most rock music in our dataset.
-Write a query that returns the Artist name and total track count of the top 10 rock bands */
-SELECT * FROM ARTIST
-SELECT * FROM ALBUM
-
-SELECT ARTIST.ARTIST_ID, ARTIST.NAME, COUNT(ALBUM.ALBUM_ID) FROM ARTIST
-JOIN ALBUM ON ARTIST.ARTIST_ID=ALBUM.ALBUM_ID
+	  Write a query that returns the Artist name and total track count of the top 10 rock bands */
+SELECT ARTIST.ARTIST_ID, ARTIST.NAME, COUNT(ARTIST.ARTIST_ID) AS NUMBER_OF_SONGS
+FROM ARTIST
+JOIN ALBUM ON ARTIST.ARTIST_ID=ALBUM.ARTIST_ID
 JOIN TRACK ON ALBUM.ALBUM_ID=TRACK.ALBUM_ID
 JOIN GENRE ON TRACK.GENRE_ID=GENRE.GENRE_ID
 WHERE GENRE.NAME ILIKE 'ROCK'
 GROUP BY ARTIST.ARTIST_ID
-ORDER BY COUNT(ALBUM.ALBUM_ID) DESC
-LIMIT 10
+ORDER BY NUMBER_OF_SONGS DESC
+LIMIT 10 
+
+/* 3. Return all the track names that have a song length longer than the average song length.
+	  Return the Name and Milliseconds for each track.
+	  Order by the song length with the longest songs listed first. */
+SELECT NAME, MILLISECONDS FROM TRACK
+WHERE MILLISECONDS > (SELECT AVG(MILLISECONDS) FROM TRACK)
+ORDER BY MILLISECONDS DESC
+
+
+
+
+
+
+
+
 
 
 
